d08f936706567b4062dcbd2b581579e5b0f2c382
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-06-07 00:50:42 +0200

    setting application.properties

--- src/main/java/guru/sfg/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -2,7 +2,9 @@ package guru.sfg.msscbreweryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
 
